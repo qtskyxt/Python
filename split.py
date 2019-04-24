6731ccebca16586ca6cc345c6da1e7178d81c9ca
Tian <qtskyxt@gmail.com>
2019-04-24 19:21:39 +0900

    split

--- split.py
+++ split.py
@@ -4,12 +4,12 @@ lines=HSC.readlines()
 ra=[]
 dec=[]
 for i in range(len(lines)):
-    t=lines.split(',')
+    t=lines[i].split(',')
     ra.append(t[0])
     dec.append(t[1])
 RA=open(pwd+'result/ra.txt','w')
 DEC=open(pwd+'result/dec.txt','w')
-for i in range(len(HSC)):
-    RA.write(ra[i]+"\n")
-    DEC.write(dec[i]+"\n")
+for i in range(len(lines)-1):
+    RA.write(ra[i+1]+"\n")
+    DEC.write(dec[i+1]+"\n")
 
