127b18716fe41b34cb274a4b2def636f62207bdb
qtskyxt <qtskyxt@gmail.com>
2019-04-24 19:34:36 +0900

    split data

--- split.py
+++ split.py
@@ -1,15 +1,13 @@
 pwd='/home/tian.qiu/catalog/'
 HSC=open(pwd+'213641.csv','r')
 lines=HSC.readlines()
-ra=[]
-dec=[]
-for i in range(len(lines)):
-    t=lines[i].split(',')
-    ra.append(t[0])
-    dec.append(t[1])
-RA=open(pwd+'result/ra.txt','w')
-DEC=open(pwd+'result/dec.txt','w')
+names=lines[0].split(',')
+files=[]
+for i in range(len(names)):
+    files.append(open(pwd+'result/'+names[i]+'.txt','w'))
 for i in range(len(lines)-1):
-    RA.write(ra[i+1]+"\n")
-    DEC.write(dec[i+1]+"\n")
+    t=lines[i+1].split(',')
+    for j in range(len(names)):
+        files[j].write(t[j]+"\n")
+
 
