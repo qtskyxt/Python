cf3b00ead42a8e3c4b96cd0e4e9fdfe780be6c42
qtskyxt <qtskyxt@gmail.com>
2019-04-24 23:29:01 +0900

    2

--- magerr-mag.py
+++ magerr-mag.py
@@ -26,6 +26,7 @@ e=(e2-e1)/6
 nerr=[e1,e1+e,e1+2*e,e1+3*e,e1+4*e,e1+5*e,e2]
 
 #对每一段区间不同光度的 star 个数进行计数 n[i][j] 表示第 i 段光度中，第 j 段 err 的个数
+'''
 for i in range(len(data)):
     for j in range(5):
 
@@ -38,6 +39,16 @@ for i in range(len(data)):
     elif data[i][0] >= nmag[3] and data[i][0] < nmag[4]:
 
     else:
+'''
+adata=np.array(data)
+n=[[0]*6 for _ in range(5)]
+for i in range(5):
+    for j in range(6):
+        t1=adata[adata[:,0]>=m[i]]
+        t2 = t1[t1[:, 0] <= m[i+1]]
+        t3 = t2[t2[:, 1] >= e[j]]
+        t4 = t1[t1[:, 1] <= m[j+1]]
+        n[i][j]=len(t4)
 
 #创建窗口
 plt.figure()
@@ -45,15 +56,17 @@ plt.figure()
 #创建子图
 plt.subplot(111)
 
-#柱子个数
-N=5
-
-
+bar_width=0.1
 
 #坐标
 plt.xlabel('mag_err')
 plt.ylabel('N')
 
+x = list(range(len(num_list)))
+for i in range(5):
+    plt.bar(6+i*bar_width, n[i], width=bar_width, label=i+1)
+plt.legend()
+plt.show()
 
 
 
