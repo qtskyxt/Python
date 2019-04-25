f4fa777015d9f5f83ae3110af68e89bf765e52d9
Tian <qtskyxt@gmail.com>
2019-04-25 22:43:46 +0900

    useless

--- magerr-mag.py
+++ magerr-mag.py
@@ -20,11 +20,11 @@ print(nmag)
 #sorted(data,key=lambda x:x[0])
 #data2=[data[i:i+math.ceil(len(data)/5)] for i in range(0,len(data),math.ceil(len(data)/5))]
 
-#按 error 的取值分成 6 段区间
+#按 error 的取值分成 10 段区间
 e1=min(magerrdata)
 e2=max(magerrdata)
-e=pow((e2/e1),1/6)
-nerr=[e1,e1*e,e1*e*e,e1*e*e*e,e2/e/e,e2/e,e2]
+e=pow((e2/e1),1/10)
+nerr=[e1,e1*e,e1*e*e,e1*e*e*e,e1*e*e*e*e,e1*e*e*e*e*e,e2/e/e/e/e,e2/e/e/e,e2/e/e,e2/e,e2]
 print(e1,e2)
 print(nerr)
 
@@ -44,9 +44,9 @@ for i in range(len(data)):
     else:
 '''
 adata=np.array(data)
-n=[[0]*6 for _ in range(5)]
+n=[[0]*10 for _ in range(5)]
 for i in range(5):
-    for j in range(6):
+    for j in range(10):
         t1=adata[adata[:,0]>=nmag[i]]
         t2 = t1[t1[:, 0] <= nmag[i+1]]
         t3 = t2[t2[:, 1] >= nerr[j]]
@@ -54,7 +54,7 @@ for i in range(5):
         n[i][j]=len(t4)
 print(n)
 #创建窗口
-plt.figure(figsize=(8,6),dpi=300)
+plt.figure(figsize=(8,10),dpi=300)
 
 #创建子图
 plt.subplot(111)
@@ -66,10 +66,10 @@ plt.title('magnitude error distribution with different i_magnitude')
 
 #坐标
 plt.xlabel('mag_err')
-nerrl=[[]]*7
-for i in range(7):
-	nerrl[i]=round(nerr[i],7)
-plt.xticks([-0.1,0.9,1.9,2.9,3.9,4.9,5.9],nerrl )
+nerrl=[[]]*11
+for i in range(11):
+	nerrl[i]=round(nerr[i],11)
+plt.xticks([-0.1,0.9,1.9,2.9,3.9,4.9,5.9,6.9,7.9,8.9,9.9],nerrl )
 plt.ylabel('N')
 plt.yscale('log')
 
@@ -78,7 +78,7 @@ for i in range(5):
     plt.bar(index+i*bar_width, n[i], width=bar_width, label='i_mag='+str(nmag[i])+'~'+str(nmag[i+1]))
 
 plt.legend()
-plt.savefig('1.png')
+plt.savefig('2.png')
 
 
 
