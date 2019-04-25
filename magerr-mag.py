9d0c7a4d717049765bf37a77f8a9938969aec8df
Tian <qtskyxt@gmail.com>
2019-04-25 22:35:39 +0900

    make it robust

--- magerr-mag.py
+++ magerr-mag.py
@@ -10,21 +10,21 @@ magerrdata=[float(x) for x in magerr.readlines()]
 
 data=[[a,b] for a,b in zip(magdata,magerrdata)]
 
-#按光度排序并分成 5 份
+#按光度排序并分成 n 份
+n=5
 m1=min(magdata)
 m2=max(magdata)
-m=(m2-m1)/5
-nmag=[m1,m1+m,m1+2*m,m1+3*m,m1+4*m,m2]
+nmag=np.linspace(m1,m2,n+1)
 print(nmag)
 
 #sorted(data,key=lambda x:x[0])
 #data2=[data[i:i+math.ceil(len(data)/5)] for i in range(0,len(data),math.ceil(len(data)/5))]
 
-#按 error 的取值分成 6 段区间
+#按 error 的取值分成 m 段等比区间
+m=10
 e1=min(magerrdata)
 e2=max(magerrdata)
-e=pow((e2/e1),1/6)
-nerr=[e1,e1*e,e1*e*e,e1*e*e*e,e2/e/e,e2/e,e2]
+nerr=np.geomspace(e1,e2,m+1)
 print(e1,e2)
 print(nerr)
 
@@ -44,9 +44,9 @@ for i in range(len(data)):
     else:
 '''
 adata=np.array(data)
-n=[[0]*6 for _ in range(5)]
-for i in range(5):
-    for j in range(6):
+n=[[0]*m for _ in range(n)]
+for i in range(n):
+    for j in range(m):
         t1=adata[adata[:,0]>=nmag[i]]
         t2 = t1[t1[:, 0] <= nmag[i+1]]
         t3 = t2[t2[:, 1] >= nerr[j]]
@@ -54,31 +54,32 @@ for i in range(5):
         n[i][j]=len(t4)
 print(n)
 #创建窗口
-plt.figure(figsize=(8,6),dpi=300)
+plt.figure(figsize=(10,6),dpi=300)
 
 #创建子图
 plt.subplot(111)
 
 bar_width=0.2
-index=np.arange(6)
+index=np.arange(m)
+xindex=range(m+1)
 
 plt.title('magnitude error distribution with different i_magnitude')
 
 #坐标
 plt.xlabel('mag_err')
-nerrl=[[]]*7
-for i in range(7):
-	nerrl[i]=round(nerr[i],7)
-plt.xticks([-0.1,0.9,1.9,2.9,3.9,4.9,5.9],nerrl )
+nerrl=[[]]*(m+1)
+for i in range(m+1):
+	nerrl[i]=round(nerr[i],5)
+plt.xticks([xindex-0.1,nerrl )
 plt.ylabel('N')
 plt.yscale('log')
 
 #画图
-for i in range(5):
+for i in range(n):
     plt.bar(index+i*bar_width, n[i], width=bar_width, label='i_mag='+str(nmag[i])+'~'+str(nmag[i+1]))
 
 plt.legend()
-plt.savefig('1.png')
+plt.savefig('2.png')
 
 
 
