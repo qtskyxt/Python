e46d78b694b4438e3022790238002c481e6128c3
Tian <qtskyxt@gmail.com>
2019-04-27 00:12:10 +0900

    final edition of magerrmean

--- magerrmean-mag.py
+++ magerrmean-mag.py
@@ -18,10 +18,10 @@ print(nmag)
 #计算每个光度区间的 error 的平均值 merr
 merr=[0]*n
 adata=np.array(data)
-for i in range(n)
-    t1=adata[adata[:,0]>=nmag[i]]
-    t2=t1[t1[:,0]<=nmag[i+1]]
-    merr[i]=np.mean(t2[1])
+for i in range(n):
+	t1=adata[adata[:,0]>=nmag[i]]
+	t2=t1[t1[:,0]<=nmag[i+1]]
+	merr[i]=np.mean(t2[:,1])
 print(merr)
 
 #创建窗口
@@ -32,22 +32,22 @@ plt.subplot(111)
 
 bar_width=1
 index=np.arange(n)
-index=index[0::3]
+xindex=np.arange(n+1)
+xindex=xindex[0::3]
 
 plt.title('magnitude error mean distribution with different i_magnitude')
 
 #坐标
 plt.xlabel('magnitude')
 for i in range(n+1):
-	nmag[i]=round(nmag[i],4)
+	nmag[i]=round(nmag[i],3)
 nmagl=nmag[0::3]
-plt.xticks(index-0.5,nmagl)
+plt.xticks(xindex-0.5,nmagl)
 plt.ylabel('mag error mean')
 #plt.yscale('log')
 
 
 #画图
-plt.bar(index, merr, width=bar_width)
+plt.bar(index, merr, width=bar_width,edgecolor='black')
 
-plt.legend()
 plt.savefig('4.png')
