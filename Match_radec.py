972c26f2f24c248d05cee7403e9afa31f93dd9c1
Tian <qtskyxt@gmail.com>
2019-05-06 23:46:24 +0900

    1st edition of Match radec, after debugging

--- Match_radec.py
+++ Match_radec.py
@@ -6,28 +6,27 @@ PI=3.14159265
 
 pwd='/home/tian.qiu/catalog/'
 
-#读取 HSC 数据，1ra 2dec 3-6g,r,i,z 7-10g,r,i,z err 0增加编码
-HSC=np.loadtxt(pwd+'213641.csv',delimiter=',')
+#读取 HSC 数据，1ra 2dec 3-6g,r,i,z 7-10g,r,i,z err 0增加编码 先只读取位置和 r,i re,ie
+HSC=np.loadtxt(pwd+'213641.csv',usecols=(0,1,4,8),delimiter=',')
 HSC=np.insert(HSC,0,range(HSC.shape[0]),axis=1)
 
-#读取 S82 数据，1ra 2dec 3rExt 4-8u,g,r,i,z 9-13u,g,r,i,z err 0 增加编码
-S82=np.loadtxt(pwd+'S82coaddStars.dat')
+#读取 S82 数据，1ra 2dec 3rExt 4-8u,g,r,i,z 9-13u,g,r,i,z err 0 增加编码 
+S82=np.loadtxt(pwd+'S82coaddStars.dat',usecols=(0,1,6,11))
 S82=np.insert(S82,0,range(S82.shape[0]),axis=1)
 
 #创建切片的数组，切片总数据为 360 份，根据 ra 每度为一份，加快运行
 HSCs=[[] for i in range(360)]
 S82s=[[] for i in range(360)]
 for i in range(HSC.shape[0]):
-    HSCs[int(HSC[i][0])].append(HSC[i])
+    HSCs[int(HSC[i][1])].append(HSC[i])
 for i in range(S82.shape[0]):
-    S82s[int(S82[i][0])].append(HSC[i])
-
+    S82s[int(S82[i][1])].append(S82[i])
 
 #定义函数，判断两者是否在 1 角秒的范围内，返回 T or F
-def dis(i,j):
-    d2 = (HSC[i][0] - S82[j][0]) ** 2 + (HSC[i][1] - S82[j][1]) ** 2
+def dis(i,j,k):
+    d2 = (HSCs[k][i][1] - S82s[k][j][1]) ** 2 + (HSCs[k][i][2] - S82s[k][j][2]) ** 2
     # 距离小于 1 角秒，pi/180/3600
-    if d2 <= PI / 180 / 3600:
+    if d2 <= (PI / 180 / 3600)**2:
         return True
     else:
         return False
@@ -37,13 +36,14 @@ match=open(pwd+'match.txt','w')
 
 #根据HSC每一行的目标寻找匹配的Stripe82中的目标
 for k in range(360):
-    for i in range(HSCs[k].shape[0]):
-        for j in range(S82s[k].shape[0]):
-            n=0
-            if dis(i,j):
-                n=n+1
-                match.write(str(j)+' ')
-        match.write(str(n)+'\n')
+	for i in range(len(HSCs[k])):
+		match.write(str(int(HSCs[k][i][0]))+' ')
+		n=0
+		for j in range(len(S82s[k])):
+			if dis(i,j,k):
+				n=n+1
+				match.write(str(int(S82s[k][j][0]))+' ')
+		match.write(str(n)+'\n')
 match.close()
 
 
