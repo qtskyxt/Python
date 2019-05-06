b1063319b6b7d238aca1147fc0d46c534b28925a
qtskyxt <qtskyxt@gmail.com>
2019-05-06 21:39:10 +0900

    match 1st edition

---
+++ Match_radec.py
@@ -0,0 +1,52 @@
+#match HSC 与 Stripe82 中的相同 star，匹配后转移到另一个文件中
+
+import numpy as np
+
+PI=3.14159265
+
+pwd='/home/tian.qiu/catalog/'
+
+#读取 HSC 数据，1ra 2dec 3-6g,r,i,z 7-10g,r,i,z err 0增加编码
+HSC=np.loadtxt(pwd+'213641.csv',delimiter=',')
+HSC=np.insert(HSC,0,range(HSC.shape[0]),axis=1)
+
+#读取 S82 数据，1ra 2dec 3rExt 4-8u,g,r,i,z 9-13u,g,r,i,z err 0 增加编码
+S82=np.loadtxt(pwd+'S82coaddStars.dat')
+S82=np.insert(S82,0,range(S82.shape[0]),axis=1)
+
+#创建切片的数组，切片总数据为 360 份，根据 ra 每度为一份，加快运行
+HSCs=[[] for i in range(360)]
+S82s=[[] for i in range(360)]
+for i in range(HSC.shape[0]):
+    HSCs[int(HSC[i][0])].append(HSC[i])
+for i in range(S82.shape[0]):
+    S82s[int(S82[i][0])].append(HSC[i])
+
+
+#定义函数，判断两者是否在 1 角秒的范围内，返回 T or F
+def dis(i,j):
+    d2 = (HSC[i][0] - S82[j][0]) ** 2 + (HSC[i][1] - S82[j][1]) ** 2
+    # 距离小于 1 角秒，pi/180/3600
+    if d2 <= PI / 180 / 3600:
+        return True
+    else:
+        return False
+
+#创建记录文件，记录匹配的总数和编号
+match=open(pwd+'match.txt','w')
+
+#根据HSC每一行的目标寻找匹配的Stripe82中的目标
+for k in range(360):
+    for i in range(HSCs[k].shape[0]):
+        for j in range(S82s[k].shape[0]):
+            n=0
+            if dis(i,j):
+                n=n+1
+                match.write(str(j)+' ')
+        match.write(str(n)+'\n')
+match.close()
+
+
+
+
+
