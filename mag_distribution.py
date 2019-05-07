6ddf98e0628cbe16ffcc98e5196f9890dba919f9
Tian <qtskyxt@gmail.com>
2019-05-08 00:37:10 +0900

    mag_distribution

--- mag_distribution.py
+++ mag_distribution.py
@@ -5,11 +5,14 @@ import matplotlib.pyplot as plt
 
 pwd='/home/tian.qiu/catalog/'
 HSC=np.loadtxt(pwd+'cutHSC',usecols=(0,1,4),delimiter=',')
+S82=np.loadtxt(pwd+'S82coaddStars.dat',usecols=(0,1,6))
 
 plt.figure(figsize=(8,6),dpi=300)
 plt.subplot(111)
-plt.hist(HSC[:,2],bins=20)
-
+bins1=plt.hist(S82[:,2],bins=20,color='red',label='S82',align='mid',rwidth=0.5)[1]
+plt.hist(HSC[:,2],bins=bins1,color='blue',label='HSC',align='left',rwidth=0.5)
 plt.title('magnitude distribution')
 plt.ylabel('N')
 plt.xlabel('mag')
+plt.legend()
+plt.savefig(pwd+'mag_dist.png')
