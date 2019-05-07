df0d0776cb86bfbe28495b6e914ef0607b136c4d
qtskyxt <qtskyxt@gmail.com>
2019-05-08 00:58:14 +0900

    ra_dec dist

---
+++ ra_dec_distribution.py
@@ -0,0 +1,25 @@
+#画出未匹配的 HSC 中 star 的位置分布
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+pwd='/home/tian.qiu/catalog/'
+HSC=np.loadtxt(pwd+'213641.csv',usecols=(0,1),delimiter=',')
+HSC=np.insert(HSC,0,range(HSC.shape[0]),axis=1)
+
+match=np.loadtxt(pwd+'match1s.txt')
+unmatch=[]
+for i in range(len(match)):
+    if match[i][1]==0:
+        unmatch.append([HSC[match[i][0]+1][0],HSC[match[i][0]+1][1]])
+
+unmatch=np.array(unmatch)
+
+plt.figure(figsize=(8,6),dpi=300)
+plt.scatter(unmatch[:,0],unmatch[:,1])
+plt.xlabel('ra')
+plt.ylabel('dec')
+plt.title('dec vs ra distribution')
+plt.savefig(pwd+'ra_dec_dist.png')
+
