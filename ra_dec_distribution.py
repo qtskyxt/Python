6760a8ff71d8fcc5c89938a35a8b34b6b52e6285
qtskyxt <qtskyxt@gmail.com>
2019-05-09 14:54:06 +0900

    radec dist modified

--- ra_dec_distribution.py
+++ ra_dec_distribution.py
@@ -4,20 +4,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-pwd='/home/tian.qiu/data/catalog/'
-HSC=np.loadtxt(pwd+'213641.csv',usecols=(0,1),delimiter=',')
+pwd1='/home/tian.qiu/data/catalog/'
+pwd2=='/home/tian.qiu/data/plot/'
+HSC=np.loadtxt(pwd1+'213641.csv',usecols=(0,1),delimiter=',')
 HSC=np.insert(HSC,0,range(HSC.shape[0]),axis=1)
-S82=np.loadtxt(pwd+'S82coaddStars.dat',usecols=(0,1))
+S82=np.loadtxt(pwd1+'S82coaddStars.dat',usecols=(0,1))
 S82=np.insert(S82,0,range(S82.shape[0]),axis=1)
 
 
 plt.figure(figsize=(8,12),dpi=300)
-plt.subplot(211)
-plt.scatter(HSC[:,1],HSC[:,2])
-plt.subplot(212)
-plt.scatter(S82[:,1],S82[:,2])
+plt.title('dec vs ra distribution')
+plt.scatter(HSC[:,1],HSC[:,2],label='HSC',alpha=0.005)
+plt.scatter(S82[:,1],S82[:,2],color='red',label='S82',alpha=0.005)
+
 plt.xlabel('ra')
 plt.ylabel('dec')
 plt.title('dec vs ra distribution')
-plt.savefig(pwd+'ra_dec_dist.png')
+plt.legend()
+plt.savefig(pwd2+'ra_dec_dist.png')
 
