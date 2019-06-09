5bc029eb557bf7f96de11807e67c366b7ec6fefa
qtskyxt <qtskyxt@gmail.com>
2019-06-09 15:22:38 +0900

    modified mag_dist.py to plot step for all and matched and unmatched HSC and S82 numbers.

--- mag_dist.py
+++ mag_dist.py
@@ -2,17 +2,25 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
-pwd1='/home/tian.qiu/data/catalog/'
-pwd2='/home/tian.qiu/data/plot/'
-HSC=pd.read_csv(pwd1+'olHSC')
-S82=pd.read_csv(pwd1+'olS82',sep='\s+')
-
+pwd1='/home/tian.qiu/data/result/6_6/'
+pwd2='/home/tian.qiu/data/catalog/6_6/'
+pwd3='/home/tian.qiu/data/plot/6_8/'
+mHSC=pd.read_csv(pwd1+'matchedHSC')
+mS82=pd.read_csv(pwd1+'matchedS82')
+umHSC=pd.read_csv(pwd1+'unmatchedHSC')
+umS82=pd.read_csv(pwd1+'unmatchedS82')
+HSC=pd.read_csv(pwd2+'HSCol.csv')
+S82=pd.read_csv(pwd2+'S82ol.csv')
 plt.figure(figsize=(8,6),dpi=300)
 plt.subplot(111)
-bins1=plt.hist(S82.iM,bins=40,color='red',label='S82',histtype='step')[1]
-plt.hist(HSC.i_cmodel_mag,bins=bins1,color='blue',label='HSC',histtype='step')
+bins1=plt.hist(mHSC.i_cmodel_mag,bins=50,color='red',label='matchedHSC',histtype='step')[1]
+plt.hist(mS82.psfMag_i,bins=bins1,color='blue',label='matchedS82',histtype='step')
+plt.hist(umS82.psfMag_i,bins=bins1,color='orenge',label='unmatchedS82',histtype='step')
+plt.hist(mS82.i_cmodel_mag,bins=bins1,color='yellow',label='unmatchedHSC',histtype='step')
+plt.hist(S82.psfMag_i,bins=bins1,color='black',label='allS82',histtype='step')
+plt.hist(HSC.i_cmodel_mag,bins=bins1,color='pink',label='allHSC',histtype='step')
 plt.title('magnitude distribution')
 plt.ylabel('N')
 plt.xlabel('mag')
 plt.legend()
-plt.savefig(pwd2+'mag_dist_ol.png')
+plt.savefig(pwd3+'mag_dist_ol.png')
